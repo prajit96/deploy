60819030d32bb6ae4fc728131cc7328c53dcc554
prajit96 <santraprajit@gmail.com>
2023-06-10 20:23:00 +0530

    learn to deploy

---
+++ index.js
@@ -0,0 +1,22 @@
+const express =require("express")
+const {connection} =require("./db")
+const {userRouter}=require("./routes/user.routes")
+const {noteRouter}=require("./routes/note.routes")
+require("dotenv").config()
+
+const app = express()
+app.use(express.json())
+
+app.use("/users",userRouter)
+app.use("/notes",noteRouter)
+
+app.listen(process.env.port,async()=>{
+    try{
+        await connection
+        console.log("connect to the DB");
+        console.log(`server is running at port ${process.env.port}`);
+    }catch(err){
+        console.log(err);
+        console.log("Something Went Wrong!!!");
+    }
+})
